5020170156b35adda7310334eb91db1e305204ba
hexrv <57159892+hexrv@users.noreply.github.com>
2020-05-07 13:36:58 -0300

    home login
    alterando

--- passosMagicos/resources/views/home.blade.php
+++ passosMagicos/resources/views/home.blade.php
@@ -8,19 +8,12 @@
         <title>Passos magicos</title>
         <link rel="stylesheet" href="css/home.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
+        
 
     </head>
 
     <body>
-        <header class="topo">
-            <img class="logo" src="/passosMagicos/storage/app/public/img/logo.png">
-            <nav>
-                <ul class="nav_links">
-                    <li><a href="#">serviços</a></li>
-                    <li><a href="#">projetos</a></li>
-                    <li><a href="#">sobre</a></li>
-                </ul>
-            </nav>
+         <header>
         <a class="cta" href="{{route('login')}}"><button>Entrar</button></a>
         <a href="#">
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -35,33 +28,88 @@
         </a>
 
         </header>
-        <div class="feed">
-            <div class="card">
-                <p class="card_category">Eventos</p>
-                <p class="card_title">Eventos mais proximos da Passos Magicos</p>
+        <section id="header">
+            <div class="container">
+                <img src="images/logon.png" class="logo" alt="">
+                <div class="header-text">
+                    <h1> Seja bem vindo a
+                        plataforma de ensino Passos magicos</h1>
+                    <span class="square"></span>
+                    <p>A passos magicos e uma plataforma de ensino não governamental
+                        e sem fins lucrativos que visa o aprendizado de forma fluida
+                        trazendo assim todo poder da tecnologia para dentro da sala
+                        e assim formando grandes profissionais e grandes pessoas
+
+                    </p>
+                    <button class="common-btn">Saiba mais..</button>
+                    <div class="line">
+                        <span class="line1"></span><br>
+                        <span class="line2"></span><br>
+                        <span class="line3"></span>
+                    </div>
+                </div>
             </div>
-            <div class="card">
-                <p class="card_category">Processo seletivo</p>
-                <p class="card_title">lost in the city is where i found myself</p>
+            <div class="feed">
+                <div class="feed--card--container">
+                    <div class="card-1">
+
+                    </div>
+                </div>
             </div>
-            <div class="card">
-                <p class="card_category">travel</p>
-                <p class="card_title">lost in the city is where i found myself</p>
+
+
+        </section>
+
+        <nav id="sideNav">
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="#quemsomos">Quem somos</a>
+                </li>
+                <li>
+                    <a href="{{route('login')}}">login</a>
+                </li>
+                <li>
+                    <a href="#footer">contatos</a>
+                </li>
+                <!-- <li>
+                <a href="">home</a>
+            </li> -->
+            </ul>
+        </nav>
+        <img src="/passosMagicos/public/storage/images/menu.png" id="menuBtn">
+
+        <section id="about">
+            <div class="about-left-col">
+                <img src="images/about.png" alt="">
+
             </div>
-            <div class="card">
-                <p class="card_category">travel</p>
-                <p class="card_title">lost in the city is where i found myself</p>
+            <div class="about-right-col">
+                <div class="about-text">
+                    <h1 id="quemsomos">Quem somos</h1>
+                    <span class="square"></span>
+
+                    <p>Somos um grupo de amigos, que se uniu para transformar a vida de crianças e adolescentes
+                        de baixa renda e alta vulnerabilidade social, através da educação.
+                        Nossa maior preocupação é com o futuro e formação desses jovens e crianças:
+                        com estudo de qualidade, as crianças são estimuladas a se desenvolverem e
+                        interagirem em um ambiente desafiador. Oferecemos ferramentas para despertar nas crianças
+                        o interesse pelo estudo e busquem/sonhem com um futuro melhor.
+                    </p><br><br>
+                    <div class="line">
+                        <span class="line1"></span><br>
+                        <span class="line2"></span><br>
+                        <span class="line3"></span>
+                    </div>
+                    <h2>Juntos somos mais fortes
+                    </h2>
+                    <h3>Frase do ano</h3>
+                </div>
             </div>
-            <div class="card">
-                <p class="card_category">travel</p>
-                <p class="card_title">lost in the city is where i found myself</p>
-            </div>
-            <div class="card">
-                <p class="card_category">travel</p>
-                <p class="card_title">lost in the city is where i found myself</p>
-            </div>
-        </div>
-        <footer>
+        </section>
+        <footer id="footer">
             <div class="middle">
                 <a class="bola" href="#">
                     <i class="fab fa-facebook-f"></i>
@@ -84,6 +132,24 @@
                 </a>
             </div>
         </footer>
+
+        <script>
+            var menubtn = document.getElementById("menuBtn");
+            var sideNav = document.getElementById("sideNav");
+
+            sideNav.style.right = "-250px";
+            menuBtn.onclick = function () {
+                if (sideNav.style.right == "-250px") {
+                    sideNav.style.right = "0";
+                } else {
+                    sideNav.style.right = "-250px";
+                }
+            }
+
+        </script>
+
+
+
     </body>
 
     </html>
--- passosMagicos/resources/views/login.blade.php
+++ passosMagicos/resources/views/login.blade.php
@@ -8,12 +8,44 @@
         <link rel="stylesheet" href="{{asset('css/style_login.css')}}">
     </head>
     <body>
+    <section id="about">
+		<div class="cobertura">
+			<div class="about-left-col">
+				<img src="/passosMagicos/public/storage/images/offer.png" alt="">
+
+			</div>
+			<div class="about-right-col">
+				<div class="login-content">
+					<form action="" method="POST">
+                    @csrf
+						<h2 class="title">Login</h2>
+						<div class="input-div one">
+							<div class="i">
+								<i class="fas fa-user"></i>
+							</div>
+							<div class="div">
+								<!-- <h5>Username</h5> -->
+								<input type="text" placeholder="usuario" class="input">
+							</div>
+						</div>
+						<div class="input-div pass">
+							<div class="i">
+								<i class="fas fa-lock"></i>
+							</div>
+							<div class="div">
+								<!-- <h5>Password</h5> -->
+								<input type="password" placeholder="Senha" class="input">
+							</div>
+						</div>
+						<a href="#">Esqueceu sua senha?</a>
+						<input type="submit" class="btn" value="Entra">
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
         <form class="box" action="" method="POST">
             @csrf
-            <h1>login</h1>
-            <input type="text" name="user" placeholder="Username">
-            <input type="password" name="senha" placeholder="password">
-            <input type="submit" name="" value="login">
             <a href="{{route('homeAdm')}}">AREA ADM</a>
         </form>
     </body>

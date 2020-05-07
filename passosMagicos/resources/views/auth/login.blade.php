8561d024e41157e91daea3d6a4f4d9d88780ffb8
verissteph <stephanieverissimopp@gmail.com>
2020-05-07 17:03:26 -0300

    login e css

--- passosMagicos/resources/views/auth/login.blade.php
+++ passosMagicos/resources/views/auth/login.blade.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
-    <link rel="stylesheet" href="{{asset('css/style_login.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/style_login.css')}}">
 </head>
+
 <body>
     <form class="box" action="" method="POST">
         @csrf

1c3f317436ee46ac05d8abac6f8f4eb6cd6f5a9d
hexrv <57159892+hexrv@users.noreply.github.com>
2020-05-08 01:47:08 -0300

    e o rei do css ataca novamente

--- passosMagicos/resources/views/auth/login.blade.php
+++ passosMagicos/resources/views/auth/login.blade.php
@@ -9,7 +9,43 @@
 </head>
 
 <body>
-    <form class="box" action="" method="POST">
+<section id="about">
+		<div class="cobertura">
+			<div class="about-left-col">
+				<img src="/passosMagicos/public/images/offer.png" alt="">
+				<!-- ta aqui a imagem senhora -->
+			</div>
+			<div class="about-right-col">
+				<div class="login-content">
+					<form action="" method="POST">
+                    @csrf
+						<h2 class="title">Login</h2>
+						<div class="input-div one">
+							<div class="i">
+								<i class="fas fa-user"></i>
+							</div>
+							<div class="div">
+								<!-- <h5>Username</h5> -->
+								<input type="text" placeholder="usuario" class="input">
+							</div>
+						</div>
+						<div class="input-div pass">
+							<div class="i">
+								<i class="fas fa-lock"></i>
+							</div>
+							<div class="div">
+								<!-- <h5>Password</h5> -->
+								<input type="password" placeholder="Senha" class="input">
+							</div>
+						</div>
+						<a href="#">Esqueceu sua senha?</a>
+						<input type="submit" class="btn" value="Entra">
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+    <!-- <form class="box" action="" method="POST">
         @csrf
         <h1>Login</h1>
         <input type="text" name="email" placeholder="email">
@@ -32,7 +68,7 @@
             </a>
           @endif
         {{-- <a href="{{route('homeAdm')}}">AREA ADM</a> --}}
-    </form>
+    </form> -->
 </body>
 </html>
 
